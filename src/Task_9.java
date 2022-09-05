705be62730e8d165db8e13667a6da579191f5095
Perkhun <iryna.perkhun@gmail.com>
2022-09-05 12:38:42 +0300

    fixFour

--- src/Task_9.java
+++ src/Task_9.java
@@ -1,11 +1,23 @@
-
 public class Task_9 {
     public static void main(String[] args) {
         CustomDouble numberOne = new CustomDouble(5, 0.2);
         CustomDouble numberTwo = new CustomDouble(7, 0.3);
-        System.out.println("Plus: " + Calculator.plus(numberOne, numberTwo));
-        System.out.println("Subtract: " + Calculator.subtract(numberOne, numberTwo));
-        System.out.println("Multiply: " + Calculator.multiply(numberOne, numberTwo));
-        System.out.println("Divide: " + Calculator.divide(numberOne, numberTwo));
+        operations(numberOne, numberTwo, '+');
+        operations(numberOne, numberTwo, '-');
+        operations(numberOne, numberTwo, '*');
+        operations(numberOne, numberTwo, '/');
+    }
+    public static void operations(CustomDouble numberOne, CustomDouble numberTwo, char operator) {
+        switch (operator) {
+            case '+' ->
+                    System.out.println(numberOne + " + " + numberTwo + " = " + Calculator.plus(numberOne, numberTwo));
+            case '-' ->
+                    System.out.println(numberOne + " - " + numberTwo + " = " + Calculator.subtract(numberOne, numberTwo));
+            case '*' ->
+                    System.out.println(numberOne + " * " + numberTwo + " = " + Calculator.multiply(numberOne, numberTwo));
+            case '/' ->
+                    System.out.println(numberOne + " / " + numberTwo + " = " + Calculator.divide(numberOne, numberTwo));
+            default -> System.out.println("ERROR");
+        }
     }
 }
